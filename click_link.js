909ce8b8393a62c3cb58fef776761409afaabb5a
ndjia <nicolep0522@gmail.com>
2026-04-27 13:23:30 -0400

    Adjusted popup window display

--- click_link.js
+++ click_link.js
@@ -1,7 +1,3 @@
-function delay(ms) {
-	return new Promise(resolve => setTimeout(resolve, ms));
-}
-
 const links = document.querySelectorAll("a");
 console.log(links);
 const selected = links[Math.floor(Math.random() * links.length)];
